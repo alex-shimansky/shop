3a149e4cd8e38c2a4c5356fd748b2c2114da2b60
AlexShimansky <shimansky77@gmail.com>
2025-05-25 14:24:17 +0300

    Изменение в новой ветке

--- resources/views/firstpage/index.blade.php
+++ resources/views/firstpage/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="min-h-screen flex flex-col">
+    Новая ветка
     <main class="flex-1 py-10 bg-gray-50">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
